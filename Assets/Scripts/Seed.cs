4f16630816eac4db7e5876eda17446271765205f
ssandoy <sanderfsandoy@hotmail.com>
2018-02-28 13:43:01 +0100

    Species and Seed-oppsett

---
+++ Assets/Scripts/Seed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed {
+
+	public string name { get; }
+	public Specie specie { get; }
+	public Sprite image { get; }
+
+	public Seed (string name, Specie specie)
+	{
+		this.name = name;
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/Specie.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Specie {
+
+	public string name { get; }
+	public int foodPoint { get; }
+	public int durability { get; }
+	public int growTime { get; }
+	public Property[] properties { get; }
+	public Sprite image { get; }
+	public int seedsPerWorker { get;}
+	public Rarity rarity { get; }
+	public bool edible { get; set;}
+
+	public Specie (string name, int foodPoint, int durability, int growTime, Property[] properties, Sprite image, int seedsPerWorker,int tag, Rarity rarity)
+	{
+		this.name = name;
+		this.foodPoint = foodPoint;
+		this.durability = durability;
+		this.growTime = growTime;
+		this.properties = properties;
+		this.seedsPerWorker = seedsPerWorker;
+		this.image = image;
+		this.tag = tag;
+		this.rarity = rarity;
+		this.edible = false;
+	}
+	
+
+	enum Property { 
+		//TODO: INSERT Properties.
+	}
+
+	enum Rarity {
+		COMMON, RARE, EPIC, LEGENDARY
+	}
+
+}
+
