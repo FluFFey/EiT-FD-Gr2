6fe9c349ad8031b7bd97b514c266dcb39df40410
Henning Einar Luick <henningel@gmail.com>
2018-02-28 15:51:42 +0100

    added early work for overworld handling

---
+++ Assets/Scripts/MouseOverObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverObj : MonoBehaviour
+{
+    public bool isMouseOver;
+
+    private void OnMouseOver()
+    {
+        isMouseOver = true;
+    }
+    private void OnMouseExit()
+    {
+        isMouseOver = false;
+    }
+}
--- Assets/Scripts/OverWorld.cs
+++ Assets/Scripts/OverWorld.cs
@@ -1,16 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Overworld : MonoBehaviour
+{
+    private const int numberOfInteractables = 6;
+    public GameObject[] children;
+    private enum highlightableObjects
+    {
+        LABORATORY,
+        FOREST,
+        FARM,
+        BUSH,
+        POND,
+        BOTTLE,
+        NO_HIGHLIGHT
+    }
+    private void Start()
+    {
+        //children = new GameObject[numberOfInteractables];
+        ////first child is 1, 0 is current gameObject
+        //for (int i = 1; i < numberOfInteractables;i++)
+        //{
+        //    children[i] = transform.GetChild(i).gameObject;
+        //}
+    }
+    void Update ()
+    {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnMouseDown()
+    {
+        highlightableObjects highlightedButton = highlightableObjects.NO_HIGHLIGHT;
+        for (int i =0; i < numberOfInteractables; i++)
+        {
+            if (children[i].GetComponent<MouseOverObj>().isMouseOver)
+            {
+                highlightedButton = (highlightableObjects)i;
+            }
+        }
+
+        switch (highlightedButton)
+        {
+            case highlightableObjects.LABORATORY:
+                SceneManager.LoadScene("Laboratory", LoadSceneMode.Single);
+                SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+                break;
+            case highlightableObjects.FOREST:
+
+                break;
+            case highlightableObjects.FARM:
+                SceneManager.LoadScene("Forest", LoadSceneMode.Single);
+                SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+                break;
+            case highlightableObjects.BUSH:
+                break;
+            case highlightableObjects.POND:
+                break;
+            case highlightableObjects.BOTTLE:
+                break;
+            case highlightableObjects.NO_HIGHLIGHT:
+                break;
+            default:
+                print("invalid highlightable object type");
+                break;
+        }
+    }
 }
