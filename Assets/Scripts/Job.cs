a2988e4ef336eb37431128b55dc2c1a0d13c2cd2
ssandoy <sanderfsandoy@hotmail.com>
2018-02-28 13:53:39 +0100

    Job-oppsett

---
+++ Assets/Scripts/Job.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Job  {
+	public int id { get; set;}
+	public int duration { get; set;}
+	public int numWorkers { get; set;}
+	public JobType jobbType;
+
+	public Job (JobType jobbType, int id, int duration, int numWorkers)
+	{
+		this.jobbType = jobbType;
+		this.id = id;
+		this.duration = duration;
+		this.numWorkers = numWorkers;
+	}	
+}
+
+public enum JobType {
+	FISH, HUNT, CLONE, SPLICE
+}
