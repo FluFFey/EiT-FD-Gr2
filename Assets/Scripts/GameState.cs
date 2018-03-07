b2be74f0a376aeb0b673593485439e4727caa0d3
ssandoy <sanderfsandoy@hotmail.com>
2018-03-07 12:09:35 +0100

    Added EnumUtil-class which return a generic random enum-value. Added NaturalDisasterClass to handle NaturalDisasters. Added CalendarLogic in GameState

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,7 +4,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+
 //TODO: REPLACE WITH GAMESTATE?
+using System;
+
+
 public class GameState : MonoBehaviour {
 
 	public static GameState instance;
@@ -17,6 +21,11 @@ public class GameState : MonoBehaviour {
 	List<Specie> unknownSpecies;
 	List<Specie> mySplices;
 
+	//CALENDAR-ATTRIBUTES
+	int totalDays;
+	int daysPassed;
+	Dictionary<int,  NaturalDisaster> naturalDisasters;
+
 	//BASE-SPECIES
 	//TODO: ADD VALUES TO EACH SPECIE. 
 	Specie[] baseSpecies = { new Specie() , new Specie(), new Specie(), new Specie(), new Specie()};
@@ -25,7 +34,9 @@ public class GameState : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		totalDays = 100;
+		setDisasters (10);
+		pressNextDay();
 	}
 	
 	// Update is called once per frame
@@ -48,14 +59,17 @@ public class GameState : MonoBehaviour {
 		this.seeds = new Dictionary<Seed, int>();
 
 		//init Species-State
-		//TODO ARRAYLIST DEPRECATED
 		this.knownSpecies = new List<Specie>(baseSpecies); 
 		this.unknownSpecies = new List<Specie>(undiscoveredSpecies); 
 		this.mySplices = new List <Specie>();
+
+		//init Calendar-State
+		this.naturalDisasters = new Dictionary<int, NaturalDisaster>();
+
+		//TODO: CALC DISASTERS.
 	}
 
-	// JOB-METHODS -------------------------------------------------------------
-
+	// -------------------------JOB-METHODS --------------------------------------
 	bool addJob(JobType jobType, int numWorkers, int id) {
 		//if insufficient survivors or available workers -> cancel job.
 		if ((survivors - this.currentJobs.Sum(j => j.numWorkers)) < numWorkers) {
@@ -74,5 +88,53 @@ public class GameState : MonoBehaviour {
 	}
 		
 
+	//------------------------CALENDAR-STATE-----------------------------
+
+	public void pressNextDay() {
+		// ADD DEATHLOGIC
+
+		// ADD ACTIVATION OF A NATURAL DISASTER IF PRESENT
+		bool val = checkDisaster(daysPassed);
+
+		// INCEREMENT DAY
+		daysPassed++;
+
+		// ADD LOGIC TO CHECK IF GAME IS WON/OVER.
+	}
+
+
+
+	//SETS PREDEFINED DISASTERS AND ADDS A RANDOM DISASTER TO EACH EVENT.
+	public void setDisasters(int interval) {
+		// FOR EACH X DAY, SET TO A RANDOM DISASTER. 
+		for (int i = interval; i <= totalDays; i += interval) {
+			Property type = EnumUtil.RandomEnumValue<Property> ();
+			switch (type) 
+			{
+			//TODO: ADD CASE FOR EACH PROPERTY THAT IS ADDED. 
+			case Property.WIND:
+				this.naturalDisasters.Add (i, new NaturalDisaster ("Hurricane", type));
+				break;
+			case Property.EARTHQUAKE:
+				this.naturalDisasters.Add (i, new NaturalDisaster ("Earthquake", type));
+				break;
+			case Property.WATER:
+				this.naturalDisasters.Add (i, new NaturalDisaster ("Tsunami", type));
+				break;
+			default:
+				print("Default case! NOTHING ADDED :((");
+				break;
+			}
+		}
+	}
+
+	//TODO: CONSIDER VOID.
+	public bool checkDisaster(int day) {
+		if (this.naturalDisasters.ContainsKey (day)) {
+			//TODO EXECUTE DISASTER LOGIC
+			return true;
+		}
+		return false;
+	}
 }
 
