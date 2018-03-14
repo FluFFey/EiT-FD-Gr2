2bbf45c828840e3cf7653d8e12013c85e50f7665
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2018-03-14 12:07:47 +0100

    Removed unnecessary local variables

--- Assets/Scripts/Cloning.cs
+++ Assets/Scripts/Cloning.cs
@@ -4,21 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cloning : MonoBehaviour{
-    private Specie specie;
-    private Seed seed;
     
     //INITIALIZE VARIABLES AND ADD TO GAMESTATE
-    //Called on click (Clone!-button)
+    //To be called on click (Clone!-button)
     public void Clone(Specie specie, int numWorkers)
     {
-        this.specie = specie;
-        seed = new Seed(specie.name + " seed", specie);
-        GameState.instance.setSeeds(seed, CalculateNumSeeds(numWorkers));
+        Seed seed = new Seed(specie.name + " seed", specie);
+        GameState.instance.setSeeds(seed, CalculateNumSeeds(specie, numWorkers));
     }
 
     //CALCULATE NUMBER OF SEEDS TO OUTPUT FROM CLONE JOB
     //TODO: Upgrade calclulation formula?
-    public int CalculateNumSeeds(int workersAllocated){
+    public int CalculateNumSeeds(Specie specie, int workersAllocated){
         return specie.seedsPerWorker * workersAllocated;
     }   
 
