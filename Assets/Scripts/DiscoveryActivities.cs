ba5f2f824975aa603d7e9686fd27efef7d3ded8b
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2018-03-21 12:03:18 +0100

    Completed MVP logic

--- Assets/Scripts/DiscoveryActivities.cs
+++ Assets/Scripts/DiscoveryActivities.cs
@@ -6,14 +6,8 @@ public class DiscoveryActivities : MonoBehaviour {
     List<Specie> knownSpecies, allSpecies, discoverableSpecies;
     static System.Random rnd = new System.Random();
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update(){
+    //initialization
+    void Awake(){
         this.allSpecies = GameState.instance.GetAllSpecies();
         this.knownSpecies = GameState.instance.GetKnownSpecies();
     }
@@ -23,9 +17,13 @@ public class DiscoveryActivities : MonoBehaviour {
         Specie discoveredSpecie = DiscoverSpecie(numWorkers);
 
         if (knownSpecies.Contains(discoveredSpecie)){
-            //TODO: Subtract discoveredSpecies.foodpoints form the total need of food for that day.
+            //Add to food points consumed for that day
+            GameState.instance.addFoodPointsConsumed(discoveredSpecie.foodPoint);
         }
-
+        else{
+            //Add known specie
+            GameState.instance.AddKnownSpecies(discoveredSpecie);
+        }
     }
 
     public void Fish(int numWorkers){
@@ -34,23 +32,29 @@ public class DiscoveryActivities : MonoBehaviour {
 
         if (knownSpecies.Contains(discoveredSpecie))
         {
-            //TODO: Subtract discoveredSpecies.foodpoints form the total need of food for that day.
+            //Add food points to food points consumed for that day
+            GameState.instance.addFoodPointsConsumed(discoveredSpecie.foodPoint);
+        }
+        else
+        {
+            //Add to known specie
+            GameState.instance.AddKnownSpecies(discoveredSpecie);
         }
     }
 
     public Specie DiscoverSpecie(int numWorkers){
         //Filter list based on numWorkers
         //TODO: TUNE RELATIONSHIP BETWEEN numWorkers and RARITY
-        //TODO: SPECIAL CASES COULD TRIGGER LEGENDARY UNINTENDEDLY
-        if(numWorkers <= 2){
+        //TODO: Consider making the if conditions dynamic with respect to numWorkers.
+        if (numWorkers <= 1){
             discoverableSpecies = FilterOnRarity(Specie.Rarity.COMMON);
-        }else if(numWorkers <= 4){
+        }else if(numWorkers <= 2){
             discoverableSpecies = FilterOnRarity(Specie.Rarity.RARE);
         }
-        else if (numWorkers <= 5){
+        else if (numWorkers <= 3){
             discoverableSpecies = FilterOnRarity(Specie.Rarity.EPIC);
         }
-        else{
+        else if (numWorkers > 3){
             discoverableSpecies = FilterOnRarity(Specie.Rarity.LEGENDARY);
         }
         //Return a random index from the filtered list
@@ -61,20 +65,50 @@ public class DiscoveryActivities : MonoBehaviour {
     //RETURNS A LIST OF SPECIES FILTERED ON THE DESIRED TYPE
     public List<Specie> FilterOnType(Specie.Type type){
         List<Specie> filteredSpecies = new List<Specie>();
-        foreach (Specie specie in discoverableSpecies)
+        discoverableSpecies.ForEach(specie =>
         {
-            filteredSpecies.Add(specie);
-        }
+            if(specie.type == type) { filteredSpecies.Add(specie); }
+        });
         return filteredSpecies;
     }
 
     //RETURNS A LIST OF SPECIES FILTERED ON THE DESIRED RARITY
     public List<Specie> FilterOnRarity(Specie.Rarity rarity){
         List<Specie> filteredSpecies = new List<Specie>();
-        foreach (Specie specie in discoverableSpecies)
-        {
-            filteredSpecies.Add(specie);
-        }
+        switch (rarity){
+            case Specie.Rarity.COMMON:
+                discoverableSpecies.ForEach(specie =>
+                {
+                    if (specie.rarity == Specie.Rarity.COMMON){
+                        filteredSpecies.Add(specie);
+                    }
+                });
+                break;
+            case Specie.Rarity.RARE:
+                discoverableSpecies.ForEach(specie =>
+                {
+                    if (specie.rarity == Specie.Rarity.COMMON || specie.rarity == Specie.Rarity.RARE){
+                        filteredSpecies.Add(specie);
+                    }
+                });
+                break;
+            case Specie.Rarity.EPIC:
+                discoverableSpecies.ForEach(specie =>
+                {
+                    if (specie.rarity == Specie.Rarity.COMMON || specie.rarity == Specie.Rarity.RARE || specie.rarity == Specie.Rarity.EPIC){
+                        filteredSpecies.Add(specie);
+                    }
+                });
+                break;
+            case Specie.Rarity.LEGENDARY:
+                discoverableSpecies.ForEach(specie =>
+                {
+                    if (specie.rarity == Specie.Rarity.RARE || specie.rarity == Specie.Rarity.EPIC || specie.rarity == Specie.Rarity.LEGENDARY){
+                        filteredSpecies.Add(specie);
+                    }
+                });
+                break;
+        }        
         return filteredSpecies;
     }
 }
